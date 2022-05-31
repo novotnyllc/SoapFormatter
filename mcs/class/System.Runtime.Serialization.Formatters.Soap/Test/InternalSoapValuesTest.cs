8380b9f91c77eada47e7eb6707a4a89959a586da
Tim Johnson <the.tim.johnson@gmail.com>
2022-05-31 11:58:40 -0400

    Fixes tests so they are version agnostic

--- mcs/class/System.Runtime.Serialization.Formatters.Soap/Test/InternalSoapValuesTest.cs
+++ mcs/class/System.Runtime.Serialization.Formatters.Soap/Test/InternalSoapValuesTest.cs
@@ -78,7 +78,7 @@ namespace SoapShared
 			string soapMessage =
 @"<SOAP-ENV:Envelope xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:SOAP-ENC=""http://schemas.xmlsoap.org/soap/encoding/"" xmlns:SOAP-ENV=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:clr=""http://schemas.microsoft.com/clr/"" SOAP-ENV:encodingStyle=""http://schemas.xmlsoap.org/soap/encoding/"">
   <SOAP-ENV:Body>
-    <a1:Serializable2Class id=""ref-1"" xmlns:a1=""http://schemas.microsoft.com/clr/nsassem/SoapShared/Test%2C%20Version%3D1.0.0.0%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3D64d05efcff27afd3"">
+    <a1:Serializable2Class id=""ref-1"" xmlns:a1=""http://schemas.microsoft.com/clr/nsassem/SoapShared/Test"">
       <m_time xsi:type=""xsd:duration"">P1DT</m_time>
     </a1:Serializable2Class>
   </SOAP-ENV:Body>
